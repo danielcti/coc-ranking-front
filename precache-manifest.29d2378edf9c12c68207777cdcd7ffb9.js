fa8da629597652d3ddfa1e3c439972e67b5c1c2b
danielcti <daniel.cti@hotmail.com>
2020-04-04 02:44:36 -0300

    Updates

---
+++ precache-manifest.29d2378edf9c12c68207777cdcd7ffb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea5572731940949a9864548520ff387",
+    "url": "/coc-ranking-front/index.html"
+  },
+  {
+    "revision": "52d47b7a6b3b4bba45be",
+    "url": "/coc-ranking-front/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "880288e1f99535031481",
+    "url": "/coc-ranking-front/static/js/2.f29cec81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coc-ranking-front/static/js/2.f29cec81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52d47b7a6b3b4bba45be",
+    "url": "/coc-ranking-front/static/js/main.0831ca3e.chunk.js"
+  },
+  {
+    "revision": "76d6d8626a5a78c60743",
+    "url": "/coc-ranking-front/static/js/runtime-main.0e7f0002.js"
+  }
+]);
